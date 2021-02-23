60125e333f5b1a5cb093e9129e872aceb9e5f294
Dmitriy Volkov <dmitriyvolkov@gmail.com>
2021-02-23 16:49:08 +0300

    durty hack for rails 6.1

--- lib/active_record/connection_adapters/clickhouse/schema_creation.rb
+++ lib/active_record/connection_adapters/clickhouse/schema_creation.rb
@@ -3,7 +3,7 @@
 module ActiveRecord
   module ConnectionAdapters
     module Clickhouse
-      class SchemaCreation < AbstractAdapter::SchemaCreation# :nodoc:
+      class SchemaCreation < ActiveRecord::SchemaMigration# :nodoc:
 
         def visit_AddColumnDefinition(o)
           sql = +"ADD COLUMN #{accept(o.column)}"
